946ec5574aa3a6f4bef0f58b93d94285e7b5991b
Armen <armen.armen.armen95@gmail.com>
2020-11-05 23:33:18 +0200

    05.11.20

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,9 @@
-// document.onreadystatechange = function() {
-//     if (document.readyState === "interactive") {
+$(document).on("readystatechange", () => {
+    document.readyState === "interactive" &&
+        $(window).width() <= 500 &&
+        $(".header__log-link").text($(".header__log-link").data("mobile-text"))
+})
 
-//     }
-// }
 $(window).on("load", () => {
     // variables
     // /variables
@@ -33,9 +34,63 @@ $(window).on("load", () => {
     //  /universal function
     // ----------------------------------------------
     // event
+    // Даты на странице poster.html
     $(".filter__container").on("input submit", (e) => {
         e.preventDefault()
         ajaxRequest("filter__container", "test.php")
+    })
+    // /Даты на странице poster.html
+    // Фильтр на странице poster.html в блоке "Кинозал"
+    $("#crumbs-one").on("input submit", (e) => {
+        e.preventDefault()
+        ajaxRequest("crumbs-one", "test.php")
+    })
+    // /Фильтр на странице poster.html в блоке "Кинозал"
+    // Фильтр на странице poster.html в блоке "Театральный зал"
+    $("#crumbs-two").on("input submit", (e) => {
+        e.preventDefault()
+        ajaxRequest("crumbs-two", "test.php")
+    })
+    // /Фильтр на странице poster.html в блоке "Театральный зал"
+    // Фильтр на странице poster.html в блоке "Концертный зал"
+    $("#crumbs-three").on("input submit", (e) => {
+        e.preventDefault()
+        ajaxRequest("crumbs-three", "test.php")
+    })
+    // /Фильтр на странице poster.html в блоке "Концертный зал"
+    // Фильтр на странице poster.html в блоке "Литературная гостиная"
+    $("#crumbs-four").on("input submit", (e) => {
+        e.preventDefault()
+        ajaxRequest("crumbs-four", "test.php")
+    })
+    // /Фильтр на странице poster.html в блоке "Литературная гостиная"
+    // Фильтр на странице poster.html в блоке "Выставка"
+    $("#crumbs-five").on("input submit", (e) => {
+        e.preventDefault()
+        ajaxRequest("crumbs-five", "test.php")
+    })
+    // /Фильтр на странице poster.html в блоке "Выставка"
+    $(".crumbs__crumb").on("click", (e) => {
+        $(e.target)
+            .parents(".crumbs__crumb")
+            .toggleClass("crumbs__crumb--active")
+        $(e.target).parents(".crumbs").children(".crumbs__crumb--active")
+            .length === 0
+            ? $(e.target)
+                  .parents(".crumbs")
+                  .children(".crumbs__clear")
+                  .removeClass("crumbs__clear--active")
+            : $(e.target)
+                  .parents(".crumbs")
+                  .children(".crumbs__clear")
+                  .addClass("crumbs__clear--active")
+    })
+    $(".crumbs__clear").on("click", function () {
+        $(this)
+            .parents(".crumbs")
+            .children(".crumbs__crumb")
+            .removeClass("crumbs__crumb--active")
+        $(this).removeClass("crumbs__clear--active")
     })
     $("#menu-element-more").on("click", () => {
         $(".menu__desktop-more-list").slideToggle(400)
@@ -58,13 +113,18 @@ $(window).on("load", () => {
         $(".filter__date-element").removeClass("filter__date-element--active")
         $(this).addClass("filter__date-element--active")
     })
+    $(".menu__burger").on("click", () => {
+        $(".menu").toggleClass("menu--open")
+    })
     pageYOffset
         ? $(".header").addClass("header--scroll")
         : $(".header").removeClass("header--scroll")
     $(window).on("scroll", () => {
         pageYOffset
-            ? $(".header").addClass("header--scroll")
-            : $(".header").removeClass("header--scroll")
+            ? ($(".header").addClass("header--scroll"),
+              $(".menu__desktop").addClass("menu__desktop--scroll"))
+            : ($(".header").removeClass("header--scroll"),
+              $(".menu__desktop").removeClass("menu__desktop--scroll"))
     })
     // /event
     // ----------------------------------------------
@@ -72,11 +132,87 @@ $(window).on("load", () => {
     // /unique function
     // ----------------------------------------------
     // Page load
-
     const swiper = new Swiper(".poster-slider__slider", {
         spaceBetween: 20,
         slidesPerView: "auto",
         grabCursor: true,
+        navigation: {
+            nextEl: "#first-slider-arrow-right",
+            prevEl: "#first-slider-arrow-left",
+        },
+        on: {
+            slideChange: ({ slides, activeIndex }) => {
+                slides.each((slide, index) => {
+                    index < activeIndex
+                        ? $(slide).addClass("swiper-slide-prev-prev")
+                        : $(slide).removeClass("swiper-slide-prev-prev")
+                })
+            },
+            reachEnd: ({ $el }) => {
+                $el.children(".poster-slider__shadow").addClass(
+                    "poster-slider__shadow--hidden"
+                )
+                $el.children(".poster-slider__dark").addClass(
+                    "poster-slider__dark--hidden"
+                )
+            },
+            fromEdge: ({ $el }) => {
+                $el.children(".poster-slider__shadow").removeClass(
+                    "poster-slider__shadow--hidden"
+                )
+                $el.children(".poster-slider__dark").removeClass(
+                    "poster-slider__dark--hidden"
+                )
+            },
+        },
     })
+    const swiperStories = new Swiper(".poster-slider__stories", {
+        spaceBetween: 10,
+        slidesPerView: "auto",
+        grabCursor: true,
+        navigation: {
+            nextEl: "#first-slider-arrow-right",
+            prevEl: "#first-slider-arrow-left",
+        },
+        breakpoints: {
+            901: {
+                spaceBetween: 25,
+            },
+            501: {
+                spaceBetween: 20,
+            },
+        },
+        on: {
+            slideChange: ({ slides, activeIndex }) => {
+                slides.each((slide, index) => {
+                    index < activeIndex
+                        ? $(slide).addClass("swiper-slide-prev-prev")
+                        : $(slide).removeClass("swiper-slide-prev-prev")
+                })
+            },
+            reachEnd: ({ $el }) => {
+                $el.children(".poster-slider__shadow").addClass(
+                    "poster-slider__shadow--hidden"
+                )
+                $el.children(".poster-slider__dark").addClass(
+                    "poster-slider__dark--hidden"
+                )
+            },
+            fromEdge: ({ $el }) => {
+                $el.children(".poster-slider__shadow").removeClass(
+                    "poster-slider__shadow--hidden"
+                )
+                $el.children(".poster-slider__dark").removeClass(
+                    "poster-slider__dark--hidden"
+                )
+            },
+        },
+    })
+    $(window).width() <= 1299 &&
+        ($(".menu__desktop-more-list")
+            .children(".menu__desktop-element")
+            .appendTo($(".menu__desktop")),
+        $("#menu-element-more").addClass("delete"))
+
     // /Page load
 })
